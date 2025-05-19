5ef27794a0bc5b8c4b526f30ddbe82ee77b4083e
Beatriz <beatriz.giovanna@gmail.com>
2025-05-19 00:47:07 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+char movendo = Convert.ToChar(Console.ReadLine()!.ToUpper());
+
+if (movendo == 'S')
+    {
+        Console.Write("Deveria (S/N)? ");
+        movendo = Convert.ToChar(Console.ReadLine()!.ToUpper());
+            if( movendo == 'S')
+             {
+                Console.WriteLine("Ótimo.");
+            }
+            else
+             {
+                Console.WriteLine("Use Silver Tape.");
+             }
+     }
+else
+    {
+        Console.Write("Deveria (S/N)? ");
+        movendo = Convert.ToChar(Console.ReadLine()!.ToUpper());
+
+            if( movendo == 'S')
+            {
+                Console.WriteLine("Use WD-40");
+            }
+            else
+            {
+                Console.WriteLine("Ótimo.");
+            }
+    }
+        
